eb32d5957b0179353d689f6bb77abb4aa958b93f
Nikita Sheverdyaev <nikita.sheverdyaev@yandex.ru>
2022-12-24 17:41:45 +0300

    feat: Add basic navigation component functionality

--- app/src/main/java/com/example/alarm86/ui/MainActivity.kt
+++ app/src/main/java/com/example/alarm86/ui/MainActivity.kt
@@ -4,18 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.commit
+import androidx.navigation.NavAction
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
 import com.example.alarm86.R
+import com.example.alarm86.databinding.ActivityMainBinding
 import com.example.alarm86.ui.fragments.AlarmManagerFragment
 
 class MainActivity : AppCompatActivity() {
     private val MAIN_ACTIVITY_TAG = "Main_activity_tag"
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        supportFragmentManager.commit {
-            add(R.id.fragment_container, AlarmManagerFragment())
-            setReorderingAllowed(true)
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         Log.d(MAIN_ACTIVITY_TAG, "MA: On create")
     }
 }
--- app/src/main/java/com/example/alarm86/ui/fragments/AlarmManagerFragment.kt
+++ app/src/main/java/com/example/alarm86/ui/fragments/AlarmManagerFragment.kt
@@ -1,19 +1,19 @@
 package com.example.alarm86.ui.fragments
 
-import android.os.Binder
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import com.example.alarm86.R
 import com.example.alarm86.databinding.FragmentAlarmManagerBinding
 
 class AlarmManagerFragment : Fragment() {
-
-    private val ALARM_MANAGER_FRAGMENT_TAG = "Alarm_manager_tag"
+    private val ALARM_MANAGER_FRAGMENT_TAG = "ALARM_MANAGER_FRAGMENT_TAG"
     private lateinit var binding: FragmentAlarmManagerBinding
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -23,4 +23,13 @@ class AlarmManagerFragment : Fragment() {
         binding = FragmentAlarmManagerBinding.inflate(inflater)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnManagerAdd.setOnClickListener {
+            Log.d(ALARM_MANAGER_FRAGMENT_TAG, "AMP: on view Created")
+            NavHostFragment.findNavController(this).navigate(R.id.action_from_manager_to_event)
+        }
+    }
+
 }
